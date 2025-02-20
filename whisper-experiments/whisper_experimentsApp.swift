a87b08d82e23a063cc0928a5c492c1ef7edbb1b1
LeoBlackCat <leonid.ivanov@gmail.com>
2025-02-20 23:19:10 +0700

    Initial Commit

---
+++ whisper-experiments/whisper_experimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_experimentsApp.swift
+//  whisper-experiments
+//
+//  Created by Leo on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct whisper_experimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whisper-experimentsTests/whisper_experimentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_experimentsTests.swift
+//  whisper-experimentsTests
+//
+//  Created by Leo on 20/02/2025.
+//
+
+import Testing
+@testable import whisper_experiments
+
+struct whisper_experimentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
